f8151b9e772092d517a96b97331c3954c7097449
Makaan <nestor.figliuolo@gmail.com>
2016-10-03 18:30:22 -0300

    Prueba

---
+++ lista.c
@@ -0,0 +1,12 @@
+#include <lista.h>
+
+//Retorna una nueva lista vacia.
+lista_t lista_crear() {
+	return lista_t = {0,null};
+}
+
+int lista_insertar(lista_t lista, unsigned int pos, int elem) {
+	if (pos==lista.cantidad_elementos) {
+		
+	}
+}
