12bff033892bc6f19a891ef19279b9729bac1adf
r <rodrigomdiaz@live.com>
2016-10-07 14:16:18 -0300

    evaasdasd

---
+++ evaluar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lista.h"
+#include "pila.h"
+
+int main(){
+    char* cadena;
+    scanf("%s", cadena);
+    printf("Cadena con espacios: %s\n",cadena);
+    eliminar_espacios(cadena);
+    printf("Cadena sin espacios: %s\n",cadena);
+    return 0;
+}
+
+void eliminar_espacios(char* cadena)
+{
+  char* cad = cadena;
+  char* aux = cadena;
+  int i=0;
+  while(*(aux+i)!=0){
+    printf("Char de aux: %c",*(aux+i));
+    i++;
+  }
+}
