7a155f1ac673dfb38dde6a6312810aaa25e5faf0
r <rodrigomdiaz@live.com>
2016-10-28 17:09:48 -0300

    verga

---
+++ test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <getopt.h>
+#include "lista.h"
+#include "pila.h"
+#include "evaluar.c"
+
+int main(){
+
+    char* cadena;
+
+    cadena="((+3 5 6 7 1))";
+    printf("%s\n",cadena);
+    apilar_cadena(cadena);
+
+
+
+
+    return 0;
+}
